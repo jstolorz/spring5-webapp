04d0433ee2a0c16185863e48b47b9d38cd0028b6
jstolorz <jstolorz@gmail.com>
2017-10-24 12:50:19 +0200

    testing jpa-entity

--- src/main/java/com/reddragon/spring5/webapp/spring5webapp/model/Author.java
+++ src/main/java/com/reddragon/spring5/webapp/spring5webapp/model/Author.java
@@ -17,9 +17,25 @@ public class Author {
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
         this.lastName = lastName;
+
         this.books = books;
     }
 
@@ -62,5 +78,15 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
